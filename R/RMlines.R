1d10f4face4246ff98337b6037b5fa784ba9d817
Charles Auerbach <auerbach@yu.edu>
2019-10-23 12:30:02 +0000

    version 1.5.10

---
+++ R/RMlines.R
@@ -0,0 +1,34 @@
+RMlines <-
+  function(behavior,x){
+    
+    targetindex<-dev.cur() 
+    targetindex<-recordPlot() 
+    
+    writeLines("-------------------------------------------------------------------------------------")
+    writeLines("Click the mouse in the gap between the phases you want the line in.")
+    writeLines("-------------------------------------------------------------------------------------")
+   # l<-locator(1)
+    
+    
+    
+    ymin=min(behavior,na.rm = TRUE)
+    ymax=max(behavior,na.rm = TRUE)
+    ymax=ymax+1
+    segments(x0=x,y0=ymin,y1=rep(ymin:ymax))
+    
+   
+    
+    
+    #u<-readline("accept line? (y/n) ")
+    #if (u=="n")
+      
+      
+    
+    #{replayPlot(targetindex)}
+    
+      #ab<-NULL
+  #ab<<-recordPlot(ab)
+    
+    
+    
+  }
